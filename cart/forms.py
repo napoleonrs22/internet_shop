4d9ef972696684c9be5db14a24a3595bbfe49c4f
Erasyl <erasylam@yandex.kz>
2025-08-06 23:26:15 +0500

    added forms

--- cart/forms.py
+++ cart/forms.py
@@ -1,3 +1,33 @@
-from  django import forms
+from wsgiref.validate import validator
+
+from django import forms
+from django.core.validators import MaxValueValidator
 from .models import CartItem
 
+class AddToCartForm(forms.Form):
+    size_id = forms.IntegerField(required=False)
+    quantity = forms.IntegerField(required=False)
+
+    def __init__(self, *args, product=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.product = product
+
+        if product:
+            sizes = product.product_size.filter(stock__gt=0)
+            if sizes.exists():
+                self.fields['size_id'] = forms.ChoiceField(
+                    choices=[(ps.id, ps.size.name) for ps in sizes],
+                    required=True,
+                    initial=sizes.first().id
+                )
+class UpdateCartItemForm(forms.ModelForm):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and self.instance.product_size:
+            max_stock = self.instance.product_size.stock
+            self.fields['quantity'].validators.append(MaxValueValidator(max_stock))
